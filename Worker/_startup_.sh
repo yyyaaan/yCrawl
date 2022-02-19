0e85f41b82bdfc19297cef6ade11609d6c8d5328
Yan Pan <yan@yan.fi>
2022-02-19 17:31:01 +0200

    Update _startup_.sh

--- Worker/_startup_.sh
+++ Worker/_startup_.sh
@@ -1,13 +1,14 @@
-#! /bin/bash
-gcloud logging write y_simple_log "test $VMID machine started" --severity="INFO" &> /home/yan/silent.log
+#!/bin/bash
+
+gcloud logging write y_simple_log "test $VMID machine started" --severity="INFO"
 AUTHKEY=$(gcloud secrets versions access latest --secret="ycrawl-simple-auth")
 
 # git pull (update to new version) - MOVED TO SYSTEM SERVICE
 
 # call the main.py and done
 cd /home/yan/yCrawl/Worker/
-nohup python3 -u main.py > /home/yan/log_$(date +"%H%M").log 2>&1 &
+nohup python3 -u main.py > /home/yan/log_$(date +"%Y%m%d_%H%M").log 2>&1 &
 
 # count possible residual files
 NRES=$(ls -1q ./Worker/cache/*.pp | wc -l)
-gcloud logging write y_simple_log "$VMID initiated (R$NRES)" --severity="INFO" &> /home/yan/silent.log
+gcloud logging write y_simple_log "$VMID initiated (R$NRES)" --severity="INFO"
