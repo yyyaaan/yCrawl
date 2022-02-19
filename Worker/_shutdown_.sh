b6c3b2958bbd2a3e2e6ea9366eb058d2f6700b0f
Yan Pan <yan@yan.fi>
2022-02-19 17:28:45 +0200

    Update _shutdown_.sh

--- Worker/_shutdown_.sh
+++ Worker/_shutdown_.sh
@@ -1,22 +1,21 @@
-#! /bin/bash
-# will not do logging if called with parameter
+#!/bin/bash
 
-gcloud logging write y_simple_log "$VMID $1" --severity="INFO" &> /home/yan/silent.log
+gcloud logging write y_simple_log "$VMID $1" --severity="INFO"
 
 ### configure path and mode carefully
 runmode=test
 cd /home/yan/yCrawl/Worker/cache
 
 ### send to cloud storage -m multitasking
-gsutil  -m \
+gsutil  -q -m \
         -h "Content-Type:text/html" \
         -h "Content-Encoding:gzip" \
         mv -z pp \
         *.pp \
-        gs://ycrawl-data/$runmode/$(date +"%Y")/$(date +"%m")/$(date +"%d") &>> /home/yan/bucketops.log
+        gs://ycrawl-data/$runmode/$(date +"%Y")/$(date +"%m")/$(date +"%d")
 
 
 ### archive to zip, -m remove files, -q quiet, bot png and pp
 ### zip -q -m $(date +"%Y%m%d_%H%M%S.zip") *.p*
 
-gcloud logging write y_simple_log "$VMID $1 upload succeeded" --severity="INFO" &> /home/yan/silent.log
+gcloud logging write y_simple_log "$VMID $1 upload succeeded" --severity="INFO"
