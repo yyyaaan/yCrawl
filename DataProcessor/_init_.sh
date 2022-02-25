547db3c63fadd225c5117c32fd24c2ac025d6402
Yan @ Tieto Mac <yan@yan.fi>
2022-02-25 16:16:13 +0200

    include pyarrow as requirements

---
+++ DataProcessor/_init_.sh
@@ -0,0 +1,11 @@
+# example for set up ycrawl-head
+
+# package dependencies as in Worker
+
+apt install python3-pip python-is-python3
+
+git clone https://github.com/yyyaaan/yCrawl
+
+cd yCrawl/DataProcessor
+pip install -r requirements.txt
+mkdir cache
